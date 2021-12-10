9641751ad609cbc2592b6903aa90554d83cf3006
KDY <rlaeodud9983@naver.com>
2021-12-10 15:30:29 +0900

    Navigation Component

--- app/src/main/java/org/gsm/software/androidnavigationgcomponent/ThirdHomeFragment.kt
+++ app/src/main/java/org/gsm/software/androidnavigationgcomponent/ThirdHomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import org.gsm.software.androidnavigationgcomponent.databinding.ThirdHomeFragmentBinding
 
 class ThirdHomeFragment : Fragment() {
@@ -17,7 +18,14 @@ class ThirdHomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         bind = DataBindingUtil.inflate(inflater,R.layout.third_home_fragment,container,false)
+
         return bind.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bind.nextBtn.setOnClickListener { findNavController().navigate(R.id.firstHomeFragment) }
+        bind.prevBtn.setOnClickListener { findNavController().navigate(R.id.secondHomeFragment) }
     }
 
 }
